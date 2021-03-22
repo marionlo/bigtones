1dc9465a01b8931674cf9d4944420d92565354e8
Marion-Lotito <marion.lotito@gmail.com>
2021-03-22 14:23:13 +0100

    edit tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,43 @@
 module.exports = {
-    purge: [],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-        extend: {},
-    },
-    variants: {
-        extend: {},
-    },
-    plugins: [],
-};
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+
+     extend: {
+      fontFamily: {
+        'inter': ['Inter', 'sans-serif'],
+      },
+      fontSize: {
+        '10xl': '10rem',
+        '75xl' :'7.5rem',
+       },
+      maxWidth: {
+        'hero': '50.75rem',
+        'intro' : '42.188rem',
+        'cat' : '75.406rem',
+       },
+       padding: {
+        '53' : '12.813rem',
+        '54': '13.375rem',
+       },
+       inset: {
+        '90': '23rem',
+       },
+       colors: {
+        ladies: {
+          DEFAULT: '#FFCDC1',
+        },
+        men: {
+          DEFAULT: '#CBE9FF',
+        },
+        children: {
+          DEFAULT: '#E0FCB5',
+        },
+     },
+     },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
